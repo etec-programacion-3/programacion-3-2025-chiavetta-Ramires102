d6c2df5fc91e2a43dee60e9621d4c6b403b5258c
Santino Amico <s.amico@alumno.etec.um.edu.ar>
2025-10-23 16:48:59 -0300

    funcionamiento de las rutas de las clases

--- Backend/models/modelos.py
+++ Backend/models/modelos.py
@@ -8,3 +8,27 @@ class Usuario:
         self.edad = edad
         self.contrasena = contrasena
         self.fecha_creacion = datetime.now()
+
+
+class Clase:
+    def __init__(self, nombre, descripcion, duracion, fecha_horario_al_que_va):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.duracion = duracion
+        self.fecha_horario_al_que_va = fecha_horario_al_que_va
+
+
+class ClaseProgramada:
+    def __init__(
+        self,
+        nombre_entrenador,
+        email_entrenador,
+        nombre_clase,
+        duracion,
+        fecha_horario_al_que_va,
+    ):
+        self.nombre_entrenador = nombre_entrenador
+        self.email_entrenador = email_entrenador
+        self.nombre_clase = nombre_clase
+        self.duracion = duracion
+        self.fecha_horario_al_que_va = fecha_horario_al_que_va
